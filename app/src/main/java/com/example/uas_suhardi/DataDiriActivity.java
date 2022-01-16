b14e908de18ced3c738fc4f344841337512d8a32
Suhardi1212 <wwardy95@gmail.com>
2022-01-16 19:02:53 +0800

    uas

---
+++ app/src/main/java/com/example/uas_suhardi/DataDiriActivity.java
@@ -0,0 +1,22 @@
+package com.example.uas_suhardi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DataDiriActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profil);
+        Intent pindah = getIntent();
+    }
+}
+
